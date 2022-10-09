117da0fa7ab7eac8be38896fc84a2962b1606725
kuchayaashiq <kucchayaashiq@gmail.com>
2022-10-09 23:04:42 +0530

    polymorphism code Added

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-
+    public static void main(String[] args) {
+        Product p =  new Product();
+       int ans1 =  p.product(1,2);
+        int ans2 = p.product(1, 2, 3);
+        double ans3 = p.product(1d,2d);
+        System.out.println(ans1+" "+ans2+" "+ans3);
+    }
+    static class Product{
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+            return x*y*z;
+        }
+        public double product(double x, double y) {
+            return x*y;
+        }
+    }
 }
